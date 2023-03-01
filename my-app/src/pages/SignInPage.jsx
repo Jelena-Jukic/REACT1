547cb178084d7f7bc21102e9923476032d35a3f3
Jelena Jukic Antolovic <j-jukic@hotmail.com>
2023-03-01 19:34:58 +0100

    commit

---
+++ my-app/src/pages/SignInPage.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+import { InputFormField } from "../components/InputFormField";
+import {SubmitFormField} from "../components/SubmitFormField";
+
+export function SignInPage () {
+    const stateArray = useState(' ');
+    const fromState = stateArray[0];
+    const setFromState = stateArray[1];
+
+    // ili umjesto prethodno: [fromState, setFromState] = useState (' ');
+
+
+    function handleSubmit (event){
+        event.preventDefault();
+        console.log (fromState);
+    }
+function handleUsernameChange (value){
+    setFromState(value);
+}
+
+    return (
+        <div className="sign-in-form">
+            <div className="card">
+            <form className="sign-in-form" onSubmit={handleSubmit}>
+             <InputFormField label = "Username" type ="text" onChange ={handleUsernameChange}/>
+             <SubmitFormField/>  
+            </form>
+            </div>
+        </div>
+    );
+};
